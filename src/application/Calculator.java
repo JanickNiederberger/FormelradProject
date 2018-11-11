f9da6482a8c452b11f0e67f58be1a2d8f7742b62
Janick Niederberger <janick.niederberger@protonmail.com>
2018-11-11 11:11:26 +0100

    put methods in the right place

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -18,28 +18,6 @@ public class Calculator {
 		this.strom = strom;
 		this.widerstand = widerstand;
 	}	
-	public double pAusUundI (double u, double i){
-		return u * i;
-	}
-	
-	public double pAusRundI (double r, double i) {
-		return r* Math.pow(i, 2);
-	}
-	
-	public double pAusUundR (double u, double r) {
-		return Math.pow(u, 2) / r;
-		
-	public double uAusRundI(double r, double i){
-		return r * i;
-	}
-	
-	public double uAusPundI(double p, double i){
-		return p / i;
-	}
-	
-	public double uAusPundR(double p, double r){
-		return sqrt(p * r);
-	}
 	
 	public double getLeistung() {
 		return leistung;
@@ -73,6 +51,29 @@ public class Calculator {
 	
 	/* Hier die Methoden mit den Formlen hinzufügen
 	 */
+	public double pAusUundI (double u, double i){
+		return u * i;
+	}
+	
+	public double pAusRundI (double r, double i) {
+		return r* Math.pow(i, 2);
+	}
+	
+	public double pAusUundR (double u, double r) {
+		return Math.pow(u, 2) / r;
+		
+	public double uAusRundI(double r, double i){
+		return r * i;
+	}
+	
+	public double uAusPundI(double p, double i){
+		return p / i;
+	}
+	
+	public double uAusPundR(double p, double r){
+		return sqrt(p * r);
+	}
+	
 	public double iAusUundR (double u, double r) {
 		return u / r;
 	}
